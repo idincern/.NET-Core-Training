511e99147f9d1f5d665e5b0f4c375bdfb26f3d18
idincern <idincern@gmail.com>
2023-08-16 12:50:36 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller // Controller classından türetilince bir requesti karşılayabilir, response döndürebilir.
+    {
+        // Controller sınıfları içerisindeki metodlara Action Metod denir.  => gelen isteği karşılayan ve gerekli operasyonları gerçekleştiren metod.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts() // https://localhost:5001/product/getproducts bu adrese girince burası tetiklenir.
+        {
+            // Controller burada modele gider bağlanır verileri çeker verileri hazırlar ve viewe gönderir. farklı veri gönderme tipleri de vardır.
+            // gönderilen veriler viewde cshtmlde işlenir ve cliente gönderilir.
+            Product product = new Product();
+            product.Id = 0;
+            product.ProductName = string.Empty;
+            product.Quantity = 0;
+
+// 1.yol    ViewResult result = new ViewResult();
+            ViewResult result = View("GetProducts", product); // 2.yol
+            return result; // controllerın clienta response etmesi return işlemidir
+// 3.yol    return View();
+        }
+    }
+}
