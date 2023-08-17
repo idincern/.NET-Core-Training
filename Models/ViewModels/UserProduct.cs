bb303c6a523bba84f8ac721a88f260605b0ba203
idincern <idincern@gmail.com>
2023-08-17 16:24:57 +0300

    UserProduct classı oluşturuldu(viewmodel yapısını view'e gönderebilmek için TODO: => EF core)

---
+++ Models/ViewModels/UserProduct.cs
@@ -0,0 +1,10 @@
+﻿using mvc_tutorial1.Models;
+
+namespace mvc_tutorial1.Models.ViewModels
+{
+    public class UserProduct
+    {
+        public User User { get; set; }
+        public Product Product { get; set; }
+    }
+}
