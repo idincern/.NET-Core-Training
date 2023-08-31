6be138ac9d56d5cc0d17ac7873dce3cb15ef916e
idincern <idincern@gmail.com>
2023-09-01 00:23:02 +0300

    Model Binding mekanizması

---
+++ mvc_tutorial1/Controllers/ModelBindingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_tutorial1.Models;
+
+namespace mvc_tutorial1.Controllers
+{
+    public class ModelBindingController:Controller
+    {
+        public IActionResult GetDataFromUser() // httpget request
+        {
+            var model = new ModelToBeBinded()
+            {
+                Name = "Default Name",
+                Quantity = 1
+            };
+            return View(model); // bu model alttaki forma ilk değer olarak gönderilir,
+                                // form doldurulduğunda bu modeldeki propertyler doldurulur.
+        }
+
+        [HttpPost]
+        public IActionResult GetDataFromUser(ModelToBeBinded incomingModel)   // httppost request
+        {
+            // Form içindeki input nesneleri post edildiğinde bu nesnelere
+            // karşılık gelen propertyleri karşılayan nesne ile otomatik olarak bind edilirler.
+            return View(incomingModel);
+        }
+    }
+}
---
+++ mvc_tutorial1/Models/ModelToBeBinded.cs
@@ -0,0 +1,8 @@
+﻿namespace mvc_tutorial1.Models
+{
+    public class ModelToBeBinded
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
