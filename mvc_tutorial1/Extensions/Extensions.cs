4996b28eee67a46ca15f22049d0be59afca31231
idincern <idincern@gmail.com>
2023-08-31 17:36:22 +0300

    custom textbox input için extensions class'ı oluşturuldu

---
+++ mvc_tutorial1/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace mvc_tutorial1.Extensions
+{
+    static public class Extensions
+    {
+        public static IHtmlContent CustomTextBox(this IHtmlHelper htmlHelper, string name, string value, object htmlAttributes)
+        =>
+            htmlHelper.TextBox(name, value, new
+            {
+                style="background-color:green; color:white; font-weight:bold;",
+                @class= "form-input",
+                a="a",
+                b="b",
+                placeholder="Test Custom PlaceHolder"
+            });
+    }
+}
