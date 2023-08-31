5092cbce49e1dbf1bfc6229701c1b1605f251fe1
idincern <idincern@gmail.com>
2023-08-31 17:38:32 +0300

    ProductService class'ı oluşturuldu

---
+++ mvc_tutorial1/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mvc_tutorial1
+{
+    public class ProductService
+    {
+        public ProductService()
+        {
+            Console.WriteLine("Product service instanciated");
+        }
+    }
+}
