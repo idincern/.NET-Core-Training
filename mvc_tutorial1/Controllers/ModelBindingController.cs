d3824cdce543026aa2b44ea4b524f14f411e8862
dincernalbant <dincer.nalbant@agteks.com>
2023-09-01 16:44:53 +0300

    ModelBinding edited

--- mvc_tutorial1/Controllers/ModelBindingController.cs
+++ mvc_tutorial1/Controllers/ModelBindingController.cs
@@ -5,22 +5,25 @@ namespace mvc_tutorial1.Controllers
 {
     public class ModelBindingController:Controller
     {
-        //public IActionResult GetDataFromUser() // httpget request
-        //{
-        //    var model = new ModelToBeBinded()
-        //    {
-        //        Name = "Default Name",
-        //        Quantity = 1
-        //    };
-        //    return View(model); // bu model alttaki forma ilk değer olarak gönderilir,
-        //                        // form doldurulduğunda bu modeldeki propertyler doldurulur.
-        //}
+        //https://localhost:5001/modelbinding/getdatafromuser
+        public IActionResult GetDataFromUser() // httpget request
+        {
+            var model = new ModelToBeBinded()
+            {
+                Name = "Default Name",
+                Quantity = 1
+            };
+            return View(model); // bu model alttaki forma ilk değer olarak gönderilir,
+                                // form doldurulduğunda bu modeldeki propertyler doldurulur.
+        }
 
         [HttpPost]
         public IActionResult GetDataFromUser(ModelToBeBinded incomingModel)   // httppost request
         {
             // Form içindeki input nesneleri post edildiğinde bu nesnelere
             // karşılık gelen propertyleri karşılayan nesne ile otomatik olarak bind edilirler.
+            var x = incomingModel.Name;
+            var y = incomingModel.Quantity;
             return View(incomingModel);
         }
 
